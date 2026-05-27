e9fe98dec058f74dedd7260a350a725b55cf99b1
Jones-Mendes <jonesoliver99@hotmail.com>
2026-05-27 18:30:39 -0300

    MindSpace

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,15 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import{nitro} from"nitro/vite";
 
 export default defineConfig({
   plugins: [
+
     tanstackStart({
       server: { entry: "server" },
     }),
+    nitro(),
     tailwindcss(),
     tsConfigPaths(),
     // React plugin must be after tanstackStart.
