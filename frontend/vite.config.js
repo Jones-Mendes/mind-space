c45c1b7256e3ff88b2b5d21c4a095409b0f46ad5
Jones-Mendes <jonesoliver99@hotmail.com>
2026-05-27 22:12:09 -0300

    atualização

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,20 +1,12 @@
 import { defineConfig } from "vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
-import{nitro} from"nitro/vite";
 
 export default defineConfig({
   plugins: [
-
-    tanstackStart({
-      server: { entry: "server" },
-    }),
-    nitro(),
     tailwindcss(),
     tsConfigPaths(),
-    // React plugin must be after tanstackStart.
     react(),
   ],
 });
